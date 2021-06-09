6f3defc4c59c2de30ee120f8ff4594189effc4a0
Todd Carlson <todd.carlson3030@me.com>
2021-06-09 14:51:54 -0600

    Created update/delete service methods

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -16,4 +16,14 @@ export class TasksComponent implements OnInit {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task)
+    .subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
 }
